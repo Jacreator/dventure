f6610293d832fd0241df58c43e177f57abc20e1f
Jacreator <jambone.james82@gmail.com>
2021-06-15 11:43:05 +0100

    added connect page

--- app/database/connect.php
+++ app/database/connect.php
@@ -1,11 +1,25 @@
 <?php
 
-$host = 'localhost';
-$user = 'root';
-$pass = '';
-$db_name = 'kit';
+// $host = 'eu-cdbr-west-01.cleardb.com';
+// $user = 'b13959495f0da9';
+// $pass = 'aa195a45';
+// $db_name = 'dventure';
 
-$conn = new MySQLi($host, $user, $pass, $db_name);
+// mysql://b13959495f0da9:aa195a45@eu-cdbr-west-01.cleardb.com/heroku_b8f88a3745026db?reconnect=true
+
+// $conn = new MySQLi($host, $user, $pass, $db_name);
+
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+
+// Connect to DB
+$conn = new MySQLi($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 
 if ($conn->connect_error) {
     die('Database connection error: ' . $conn->connect_error);

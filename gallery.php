03687d4a46066bc167ac514d07f4f680393ea528
James Adakole <jambone.james82@gmail.com>
2021-08-19 22:34:34 +0100

    added gallery page

--- gallery.php
+++ gallery.php
@@ -16,12 +16,12 @@
                         </a>
                         <!-- ***** Logo End ***** -->
                         <!-- ***** Menu Start ***** -->
-                        <!-- <ul class="nav">
-                            <li class="scroll-to-section"><a href="#welcome" class="menu-item">Home</a></li>
-                            <li class="scroll-to-section"><a href="#about" class="menu-item">About</a></li>
-                            <li class="scroll-to-section"><a href="#testimonials" class="menu-item">Testimonials</a>
-                            </li>
-                            <li class="submenu">
+                        <ul class="nav">
+                            <li class="scroll-to-section"><a href="#security" class="menu-item">Security</a></li>
+                            <li class="scroll-to-section"><a href="#health" class="menu-item">Health Care</a></li>
+                            <!-- <li class="scroll-to-section"><a href="#testimonials" class="menu-item">Testimonials</a>
+                            </li> -->
+                            <!-- <li class="submenu">
                                 <a href="javascript:;">Drop Down</a>
                                 <ul>
                                     <li><a href="" class="menu-item">About Us</a></li>
@@ -31,12 +31,12 @@
                                 </ul>
                             </li> 
                             <li class="scroll-to-section"><a href="#contact-us" class="menu-item">Contact Us</a></li>
-                            <li class="pull-right scroll-to-section"><a href="#appointment">Book An Appointment</a></li>
+                            <li class="pull-right scroll-to-section"><a href="#appointment">Book An Appointment</a></li> -->
 
-                        </ul> 
+                        </ul>
                         <a class='menu-trigger'>
                             <span>Menu</span>
-                        </a> -->
+                        </a>
                         <!-- ***** Menu End ***** -->
                     </nav>
                 </div>
@@ -48,6 +48,32 @@
     <!-- ***** Preloader Start ***** -->
     <?php include('./includes/pageloader.php') ?>
     <!-- ***** Preloader End ***** -->
+
+    <!-- ****** Security Section start ***** -->
+    <section class="section" id="security">
+        <div class="container">
+            <div class="col-lg-8 offset-lg-2">
+                <div class="center-heading">
+                    <h2><em>Security</em> Images</h2>
+                    <p>Here is our core valuse that drive us daily and help us to make sure we do and keep doing our best to keep clients and ourselfs safe at all times </p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- ***** Security section end -->
+
+    <!-- ****** Security Section start ***** -->
+    <section class="section" id="health">
+        <div class="container">
+            <div class="col-lg-8 offset-lg-2">
+                <div class="center-heading">
+                    <h2>Health <em>Images</em></h2>
+                    <p>Here is our core valuse that drive us daily and help us to make sure we do and keep doing our best to keep clients and ourselfs safe at all times </p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- ***** Security section end -->
 
     <!-- ***** Header Area Start ***** -->
     <?php include('./includes/footer.php') ?>

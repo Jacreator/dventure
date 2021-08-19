007eb8fb99e9d26ff94f79a82a36a53b90be7cf5
James Adakole <jambone.james82@gmail.com>
2021-08-19 22:24:00 +0100

    added gallery page

--- gallery.php
+++ gallery.php
@@ -4,7 +4,45 @@
 
 <body>
     <!-- ***** Preloader Start ***** -->
-    <?php include('./includes/header.php') ?>
+    <header class="header-area header-sticky">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <nav class="main-nav">
+                        <!-- ***** Logo Start ***** -->
+                        <a href="index.php" class="logo">
+                            <!-- <img src="assets/images/kit32.png"> -->
+                            <span>DVenture</span>
+                        </a>
+                        <!-- ***** Logo End ***** -->
+                        <!-- ***** Menu Start ***** -->
+                        <!-- <ul class="nav">
+                            <li class="scroll-to-section"><a href="#welcome" class="menu-item">Home</a></li>
+                            <li class="scroll-to-section"><a href="#about" class="menu-item">About</a></li>
+                            <li class="scroll-to-section"><a href="#testimonials" class="menu-item">Testimonials</a>
+                            </li>
+                            <li class="submenu">
+                                <a href="javascript:;">Drop Down</a>
+                                <ul>
+                                    <li><a href="" class="menu-item">About Us</a></li>
+                                    <li><a href="" class="menu-item">Features</a></li>
+                                    <li><a href="" class="menu-item">FAQ's</a></li>
+                                    <li><a href="" class="menu-item">Blog</a></li>
+                                </ul>
+                            </li> 
+                            <li class="scroll-to-section"><a href="#contact-us" class="menu-item">Contact Us</a></li>
+                            <li class="pull-right scroll-to-section"><a href="#appointment">Book An Appointment</a></li>
+
+                        </ul> 
+                        <a class='menu-trigger'>
+                            <span>Menu</span>
+                        </a> -->
+                        <!-- ***** Menu End ***** -->
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </header>
     <!-- ***** Preloader End ***** -->
 
     <!-- ***** Preloader Start ***** -->

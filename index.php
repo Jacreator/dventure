afc81f0c758ddd3602424799b5b565152ccb1978
James Adakole <jambone.james82@gmail.com>
2021-08-19 21:10:14 +0100

    added gallery page

--- index.php
+++ index.php
@@ -4,30 +4,9 @@ include(ROOT_PATH . "/app/controllers/contact.php");
 include(ROOT_PATH . "/app/controllers/appointment.php");
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="jacreator">
-    <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900&display=swap" rel="stylesheet">
-    <link rel='icon' href='assets/images/' type='image/x-icon' />
-
-    <title>Dventure</title>
-
-    <!-- Additional CSS Files -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
-
-    <link rel="stylesheet" type="text/css" href="assets/css/font-awesome.css">
-
-    <link rel="stylesheet" href="assets/css/custom-style.css?v=2">
-
-    <link rel="stylesheet" href="assets/css/owl-carousel.css">
-
-</head>
+    <!-- ***** head Start ***** -->
+    <?php include('./head.php') ?>
+    <!-- ***** head End ***** -->
 
 <body>
 
@@ -35,9 +14,8 @@ include(ROOT_PATH . "/app/controllers/appointment.php");
     <?php include('./pageloader.php') ?>
     <!-- ***** Preloader End ***** -->
 
-
     <!-- ***** Header Area Start ***** -->
-    <?php include("./header.php")?>
+    <?php include("./header.php") ?>
     <!-- ***** Header Area End ***** -->
 
 

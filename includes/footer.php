7963dc73e4d9d6e5c49187a465e243e0b7d110f0
James Adakole <jambone.james82@gmail.com>
2021-09-09 23:34:07 +0100

    finished work

--- includes/footer.php
+++ includes/footer.php
@@ -49,11 +49,11 @@
                             <i class="fa fa-envelope" aria-hidden="true"></i> info@dialventures.co.uk
                             <hr>
                         </p>
-                        <ul class="social">
+                        <!-- <ul class="social">
                             <li><a href="https://fb.com/templatemo"><i class="fa fa-facebook"></i></a></li>
                             <li><a href="#"><i class="fa fa-twitter"></i></a></li>
                             <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
-                        </ul>
+                        </ul> -->
                     </div>
                 </div>
             </div>
